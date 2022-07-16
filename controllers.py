69727208f28cf8e543d50bc5f09a90c1ee0b9277
Valerii Martell <valerii.martell@gmail.com>
2022-07-17 00:26:25 +0200

    Add API get methods handling to json or xml formats with various args

--- controllers.py
+++ controllers.py
@@ -1,11 +1,55 @@
-from flask import render_template, make_response
+from flask import render_template, make_response, jsonify, request
+import xmltodict
 
 from models import XRate
 
 
-def get_all_rates():
-    try:
+class BaseController:
+    def __init__(self):
+        self.request = request
+
+    def call(self, *args, **kwds):
+        try:
+            return self._call(*args, **kwds)
+        except Exception as ex:
+            return make_response(str(ex), 500)
+
+    def _call(self, *args, **kwds):
+        raise NotImplementedError("_call")
+
+
+class ViewAllRates(BaseController):
+    def _call(self):
         xrates = XRate.select()
         return render_template("xrates.html", xrates=xrates)
-    except Exception as ex:
-        return make_response(str(ex), 500)
+
+
+class GetApiRates(BaseController):
+    def _call(self, fmt):
+        xrates = XRate.select()
+        xrates = self._filter(xrates)
+
+        if fmt == "json":
+            return self._get_json(xrates)
+        elif fmt == "xml":
+            return self._get_xml(xrates)
+        raise ValueError(f"Unknown fmt: {fmt}")
+
+    def _filter(self, xrates):
+        args = self.request.args
+
+        if "from_currency" in args:
+            xrates = xrates.where(XRate.from_currency == args["from_currency"])
+
+        if "to_currency" in args:
+            xrates = xrates.where(XRate.to_currency == args.get("to_currency"))
+
+        return xrates
+
+    def _get_xml(self, xrates):
+        d = {"xrates": {"xrate": [
+            {"from": rate.from_currency, "to": rate.to_currency, "rate": rate.rate} for rate in xrates]}}
+        return make_response(xmltodict.unparse(d), {'Content-Type': 'text/xml'})
+
+    def _get_json(self, xrates):
+        return jsonify([{"from": rate.from_currency, "to": rate.to_currency, "rate": rate.rate} for rate in xrates])
--- views.py
+++ views.py
@@ -1,12 +1,18 @@
+
 from app import app
 import controllers
 
 
 @app.route("/")
 def hello():
-    return "Golden Eye API!"
+    return "Golden Eye API"
 
 
 @app.route("/xrates")
 def view_rates():
-    return controllers.get_all_rates()
+    return controllers.ViewAllRates().call()
+
+
+@app.route("/api/xrates/<fmt>")
+def api_rates(fmt):
+    return controllers.GetApiRates().call(fmt)
